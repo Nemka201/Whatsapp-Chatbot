6999743bc17a57c0fdb2739984ce38f01fc377eb
Benjamín Rey <48369438+Nemka201@users.noreply.github.com>
2025-02-25 20:52:21 -0300

    Add proxy

--- client/src/services/InterceptorService.js
+++ client/src/services/InterceptorService.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const EXPRESS_PORT = 3000;
 const api = axios.create({
-    baseURL: `http://localhost:${EXPRESS_PORT}/api/`, 
+    // baseURL: `http://localhost:${EXPRESS_PORT}/api/`,
+    baseURL: `/api/`, 
+
 });
 
 api.interceptors.request.use(

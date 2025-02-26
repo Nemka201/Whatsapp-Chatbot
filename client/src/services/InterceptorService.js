8970eb54336215ab9e177fe88f4779b3cf48df7c
Benjamín Rey <48369438+Nemka201@users.noreply.github.com>
2025-02-25 23:12:43 -0300

    Update docker-compose

--- client/src/services/InterceptorService.js
+++ client/src/services/InterceptorService.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const EXPRESS_PORT = 3000;
 const api = axios.create({
     // baseURL: `http://localhost:${EXPRESS_PORT}/api/`,
-    baseURL: `http://server:${EXPRESS_PORT}/api/`, 
-
+    baseURL: `${window.location.origin}/api/`, // Se ajusta automáticamente según la URL del frontend
 });
 
 api.interceptors.request.use(

46218ca7c455e36c6f2622baf06dadadf2bf8af7
Benjamín Rey <48369438+Nemka201@users.noreply.github.com>
2025-03-01 17:43:28 -0300

    Update InterceptorService.js

--- client/src/services/InterceptorService.js
+++ client/src/services/InterceptorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-URL: process.env.REACT_APP_API_URL 
+const URL = process.env.REACT_APP_API_URL 
 
 const api = axios.create({
     // baseURL: `http://localhost:${EXPRESS_PORT}/api/`,

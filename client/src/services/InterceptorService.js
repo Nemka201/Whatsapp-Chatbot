7e66304f41e683fd902f9b941fe2c2c0aa681535
Benjamín Rey <48369438+Nemka201@users.noreply.github.com>
2025-03-01 16:29:57 -0300

    Update InterceptorService.js

--- client/src/services/InterceptorService.js
+++ client/src/services/InterceptorService.js
@@ -9,7 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     config => {
-        console.log('Api:', baseURL);
+        console.log('Api:', api.baseURL);
         const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
